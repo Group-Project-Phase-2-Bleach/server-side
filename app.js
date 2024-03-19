bdfb936ca730cdba6d8014126dfedda23aa277dc
carloshakim00 <carloshakim00@gmail.com>
2024-03-19 15:54:55 +0700

    Feat: register login

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ const app = express();
 
 const port = 3000;
 const cors = require("cors");
+const UserController = require("./controllers/UserController");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cors());
 
+
+app.post("/login", UserController.Login);
+app.post("/register", UserController.Register);
+app.post("/google-login", UserController.googleLogin);
+
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "HEllo world" });
+  res.status(200).json({ message: "Hello world" });
 });
+
+
 
 app.listen(port, () => {
   console.log("Server is running " + port);

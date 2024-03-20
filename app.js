957435b42680fa73a93a67ad7b7a06f82a5ce91b
raihank43 <raihankusuma5@gmail.com>
2024-03-20 08:12:01 +0700

    feat: moving socket to app js

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ if (process.env.NODE_ENV !== "production") {
   require("dotenv").config(); 
 }
 
-const port = 3000;
+// const port = 3000;
 const express = require("express");
 const app = express();
 const multer = require("multer");
 const upload = multer({ storage: multer.memoryStorage() });
 const cors = require("cors");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "https://koneksi-on.web.app",
+    methods: ["GET", "POST"],
+  },
+});
+const PORT = process.env.PORT || 3000;
 
 
 const authentication = require("./middlewares/authentication");
@@ -52,5 +62,28 @@ app.post("/group", upload.single("image"), GroupController.sendMessageToPublicGr
 app.delete("/group/:id", deletePublicMessageAuthorization, GroupController.deletePublicGroupMessage)
 
 app.use(errorHandler);
+
+
+io.on("connection", (socket) => {
+  console.log({ status: `User Connected.` });
+
+  socket.on("sendMessage", (data) => {
+
+    io.emit("broadcastMessage", data);
+  });
+
+  socket.on("deleteMessage", (data) => {
+    io.emit("broadcastDelete", data);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log("Server is running on port", PORT);
+});
+
 
 module.exports = app;
